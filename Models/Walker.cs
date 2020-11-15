1a79017bef91556f4f7a71d8ba61654c20011469
dek-haji <deeeqhaji@gmail.com>
2020-11-14 18:15:26 -0600

    first commit

---
+++ Models/Walker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogGO.Models
+{
+    public class Walker
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int NeighborhoodId { get; set; }
+        public string ImageURL { get; set; }
+        public Neighborhood Neighborhood { get; set; }
+    }
+}
